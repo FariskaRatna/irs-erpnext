0410bb7943dc3a00111ef3e12cf53587884f1294
Fariska Ratna <fariskafauziah07@students.unnes.ac.id>
2025-12-09 17:57:44 +0700

    chore:delete like and comment icon in employee column

--- erpnext/setup/doctype/employee/employee_list.js
+++ erpnext/setup/doctype/employee/employee_list.js
@@ -8,4 +8,33 @@ frappe.listview_settings["Employee"] = {
 			"status,=," + doc.status,
 		];
 	},
+
+	refresh: listview => {
+		$('span.level-item.list-liked-by-me.hidden-xs').remove();
+        $('span.list-row-like.hidden-xs').remove();
+        $('span.comment-count.d-flex.align-items-center').remove();
+        $('span.mx-2').remove();
+
+        setTimeout(() => {
+            $('.list-row .level-right').css({
+                "flex": "0 0 70px",
+                "max-width": "70px",
+                "padding": "0 4px",
+                "margin": "0",
+                "text-align": "right",
+                "white-space": "nowrap",
+                "overflow": "hidden"
+            });
+
+            $('.list-row-head .level-right').css({
+                "flex": "0 0 70px",
+                "max-width": "70px",
+                "padding": "0 4px",
+                "margin": "0",
+                "text-align": "right",
+                "white-space": "nowrap",
+                "overflow": "hidden"
+            });
+        }, 0)
+	}
 };
